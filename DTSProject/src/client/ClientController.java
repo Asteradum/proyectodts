65b2d79307620bda61c0a0ca794bdb94d79027a6
AlvaroMartinOrive <AlvaroMartinOrive@25642c70-1a3e-11df-8faf-b330578705e8>
2010-05-03 15:47:14 +0000

    Falta pulir los metodos. He estado pensando en crear una exception nueva que recoja los mensaje de protocolo en caso de error, pero ya vere como lo modifico :-)

--- DTSProject/src/client/ClientController.java
+++ DTSProject/src/client/ClientController.java
@@ -4,6 +4,7 @@ package client;
 import java.net.*;
 import java.io.*;
 import java.util.*;
+
 import data.*;
 
 /**
@@ -36,74 +37,228 @@ public ClientController(String IP) {
  }
 }
 
-/**
-* Envía las primitivas USUARIO y CLAVE al servidor con los valores apropiados.
-* @param u Nombre del usuario con el cual conectar.
-* @param c Clave con la cual conectar.
-* @return true si se ha permitido el acceso, false en caso contrario.
-*/
-public boolean entrar(String u, String c){
- try{
-   dataWriter.writeBytes("usuario " + u + "\r\n");
-   System.out.println(dataReader.readLine()); 
-   dataWriter.writeBytes("clave " + c + "\r\n");
-   String r = dataReader.readLine();
-   System.out.println(r);
-   if (r.startsWith("201"))
-     return true;
-   else
-     return false;
- }catch(IOException ioe){
-   System.err.println(ioe);
- }
- return false;
-}
+	public boolean login(String user, String pass){
+	 try{
+		 	dataWriter.writeBytes("USER " + user + "\r\n");
+			System.out.println(dataReader.readLine()); 
+			
+			dataWriter.writeBytes("PASS " + pass + "\r\n");		 
+		    String r = dataReader.readLine(); 
+		    System.out.println(r);
+		    
+		   if (r.startsWith("202"))
+		     return true;
+		   else
+		     return false;
+	 }catch(IOException ioe){
+	   System.err.println(ioe);
+	 }
+	 return false;
+	}
+	
+	public List<String> getListSensors(){
+		try {
+			List<String> sensors = new ArrayList<String>();
+			String r = null;
+			
+			dataWriter.writeBytes("LISTSENSOR\r\n");
+			r = dataReader.readLine();
+			System.out.println(r);
+			if (r.startsWith("112")){
+				r = dataReader.readLine();
+				while (!r.startsWith("212"))
+					sensors.add(dataReader.readLine());
+				
+				
+				//Si funciona borrar este for y el mensaje del numero de sensores previo del server.
+				for(int i=0;i<sensors.size();i++)
+					System.out.println(sensors.get(i));
+				
+				return sensors;
+			}
+			else return null;
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 
-/**
-* Envía la primitiva LISTA al servidor, para obtener la tabla de correspondencias del diccionario..
-* @return Un array de String con los contenidos de la tabla de correspondencias del diccionario.
+	}
+	
+	
+	public List<String> getHistoryLog(String sensor){		
+		try {
+			List<String> logs = new ArrayList<String>();
+			String r = null;
+			
+			dataWriter.writeBytes("HISTORYLOG " + sensor + "\r\n");
+			r =dataReader.readLine();
+			
+			
+			if (r.startsWith("113")){
+				r = dataReader.readLine();
+				while (!r.startsWith("213"))
+					logs.add(dataReader.readLine());
+							
+				//Si funciona borrar este for y el mensaje del numero de log previo del server.
+				for(int i=0;i<logs.size();i++)
+					System.out.println(logs.get(i));
+				
+				return logs;
+			}
+			else return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	//Todos los metodos de ON/OFF, devolver en vez de un booleano, 
+	//si ha sido correcto/no correcto/ya estaba en ese estado
+	public boolean setSensorOFF (String sensor){
+		
+		try {
+			dataWriter.writeBytes("OFF " + sensor + "\r\n");
+			String r = dataReader.readLine();
+			System.out.println(r);
+			
+			if (r.startsWith("204"))
+				return true;
+			else return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean setSensorON (String sensor){
+		
+		try {
+			dataWriter.writeBytes("ON " + sensor + "\r\n");
+			String r = dataReader.readLine();
+			System.out.println(r);
+			
+			if (r.startsWith("203"))
+				return true;
+			else return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean setGPSOFF (){
+		try {
+			dataWriter.writeBytes("GPSOFF\r\n");
+			String r = dataReader.readLine();
+			System.out.println(r);
+			
+			if (r.startsWith("206"))
+				return true;
+			else return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean setGPSON (){
+		try {
+			dataWriter.writeBytes("GPSON\r\n");
+			String r = dataReader.readLine();
+			System.out.println(r);
+			
+			if (r.startsWith("205"))
+				return true;
+			else return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public String getCurrentValue(String sensor){
+		StringTokenizer sTok= null;
+		String r = null;
+		try {
+			dataWriter.writeBytes("GET_CURVALUE " + sensor + "\r\n");
+			r = dataReader.readLine();
+			System.out.println(r);
+			if (r.startsWith("114")){
+				sTok= new StringTokenizer(r," ");
+				sTok.nextToken();sTok.nextToken();
+				r = sTok.nextToken(); 
+				return r;
+			}
+			else return "Error";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Error";
+	}
+	
+	public String getLocation(){
+		
+		StringTokenizer sTok= null;
+		String r = null;
+		try {
+			dataWriter.writeBytes("GET_LOC\r\n");
+			r = dataReader.readLine();
+			System.out.println(r);
+			if (r.startsWith("115")){
+				sTok= new StringTokenizer(r," ");
+				sTok.nextToken();sTok.nextToken();
+				r = sTok.nextToken(); 
+				return r;
+			}
+			else return r;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Error";
+	}
+	
+	
+	//Tiene que devovler la ruta donde ha dejado guardada la foto
+	//En un futuro peude que se cambie por un dato de devolucion tipo Image
+	//¿Utilizar una Excepcion nueva?
+	public String getPicture(){
+		String r = null;
+		try {
+			dataWriter.writeBytes("GET_PIC\r\n");
+			r = dataReader.readLine();
+			System.out.println(r);
+			if (r.startsWith("207")){
+				String photoData = null;
+				String output = dataReader.readLine();
+				while (!output.endsWith("bytes transmitted")){
+					photoData = output;
+					output = dataReader.readLine();
+				}
+				System.out.println(output);
+				return photoData;
+			}
+			else return r;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return r;
+		
+	}
 
-public String[] lista(){
- try{
-   Vector vs = new Vector();
-   dos.writeBytes("lista\r\n");
-   String linea = br.readLine();
-   System.out.println(linea);
-   for(;!linea.startsWith("202");){
-     vs.addElement(linea);
-     linea = br.readLine();
-     System.out.println(linea);
-   }
-   String[] as = new String[vs.size()];
-   for(int i=0;i<as.length;i++)
-     as[i] = (String)vs.elementAt(i);
-   return as;
- }catch(IOException ioe){
-   System.err.println(ioe);
- }
- return null;
-}
 
-/**
-* Envía la primitiva INCLUIR al servidor, para añadir una palabra.
-* @param pal Palabra a añadir.
-* @param trad Traducción correspondiente.
-
-public void incluir(String pal, String trad){
- try{
-   dos.writeBytes("incluir "+ pal + " " + trad + "\r\n");
-   System.out.println(br.readLine());
- }catch(IOException ioe){
-   System.err.println(ioe);
- }
-}
-*/
-/**
-* Envía la primitiva SALIR al servidor y realiza la desconexión.
-*/
-		public void salir(){
+	public void salir(){
 		 try{
-		   dataWriter.writeBytes("salir\r\n");
+		   dataWriter.writeBytes("QUIT\r\n");
 		   System.out.println(dataReader.readLine());
 		   dataWriter.close();
 		   dataReader.close();
